d9e13e95bf2a0a8bd0fa9bcd0d0cc08a3144c8ac
Nikki Gupta <nikkigupta933@gmail.com>
2025-12-09 11:41:57 +0530

    Initial backend code upload

---
+++ src/main/java/com/employee/crud/controller/EmployeeController.java
@@ -0,0 +1,120 @@
+package com.employee.crud.controller;
+
+
+import com.employee.crud.entity.Employee;
+import com.employee.crud.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+@CrossOrigin(origins="http://localhost")
+@RestController
+public class EmployeeController {
+	
+	@Autowired 
+	private EmployeeService employeeService;
+	
+	
+	
+	
+	
+	@PostMapping("/api/employee")
+	public Employee saveEmployee(@RequestBody Employee  employee) {
+		
+		return employeeService.saveEmployee(employee);
+	}
+	
+	@GetMapping("/api/employee")
+	public List<Employee> getAllEmployees(){
+		return employeeService.fetchAllEmployees();
+	}
+	@GetMapping("/api/test")
+	public String testApi() {
+	    return "API is working!";
+	}
+	
+	@GetMapping("/api/employee/{id}")
+	public Employee getEmployeeById(@PathVariable("id") Long id) {
+	return employeeService.getEmployeeById(id);
+	
+	}
+	
+	@PutMapping("/api/employee/{id}")
+	public Employee updateEmployeeById(@PathVariable("id") Long id,@RequestBody Employee employee) {	
+	return employeeService.updateEmployeeById(id, employee);
+}
+ @DeleteMapping("/api/employee/{id}")
+ public String delteEmployee(@PathVariable("id") Long id){
+	 return employeeService.deleteEmployeeById(id);
+	 
+ }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/employee/crud/service/EmployeeServiceImpl.java
@@ -0,0 +1,82 @@
+package com.employee.crud.service;
+
+import com.employee.crud.entity.Employee;
+import com.employee.crud.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+		
+		
+		
+	}
+	@Override
+	public List<Employee> fetchAllEmployees(){
+	List<Employee> allEmployees=employeeRepository.findAll();
+	return allEmployees;
+	
+	}
+	
+	@Override
+	public Employee getEmployeeById(Long id) {
+			Optional<Employee> employee= employeeRepository.findById(id);
+			if(employee.isPresent()) {
+				return employee.get();
+			}else {
+				return null;
+			}
+	
+	
+}
+	@Override
+	public Employee updateEmployeeById(Long id,Employee employee) {
+		Optional<Employee> employee1= employeeRepository.findById(id);
+		System.out.println(id);
+		if(employee1.isPresent()) {
+			Employee originalEmployee =employee1.get();
+			if(Objects.nonNull(employee.getName())&& !"".equalsIgnoreCase(employee.getName())) {
+				System.out.println(1);
+				originalEmployee.setName(employee.getName());
+		}
+		if(Objects.nonNull(employee.getDoj())&&employee.getDoj()!="") {
+			System.out.println(2);
+			originalEmployee.setDoj(employee.getDoj());
+}
+	if(Objects.nonNull(employee.getSalary())&& employee.getSalary() !=0) {
+		System.out.println(4);
+		originalEmployee.setSalary(employee.getSalary());
+}
+if(Objects.nonNull(employee.getStatus())&& employee.getStatus() !=-1) {
+	System.out.println(5);
+	originalEmployee.setStatus(employee.getStatus());
+}
+return employeeRepository.save(originalEmployee);
+}
+return null;
+}
+	@Override
+	public String deleteEmployeeById(Long id) {
+		if(employeeRepository.findById(id).isPresent()){
+			employeeRepository.deleteById(id);
+			return " Employee Deleted Successfully";
+		}
+		return "Employee ny this ID doesn't Exisit";
+		
+			
+		}
+	}
+
